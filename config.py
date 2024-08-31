af6dae9f166a909a371eda6f0a55f3a53f5a1ee7
TelegrmRxze3 <Abeharizween@gmail.com>
2024-08-31 20:27:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7522563411:AAFOfxo1x0DtP9Ns3Xh-jGPRP2s78vccedE")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Razeeq13")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1860895713")
 
 
 
